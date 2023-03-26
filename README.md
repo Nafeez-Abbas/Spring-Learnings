01e1f307a5a4d1a450d4a0fae4e813bc432978df
Nafeez Abbas <nafeezabbas@gmail.com>
2023-03-26 13:27:42 +0530

    Logger update - using lombok annotation for logger

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Spring-Learnings
 
 This is Spring Boot Project wherein I have added my learnings and PoC's
+
+
+Database connection, Basics & a simple api - 1 to 9
--- src/main/java/com/learnings/log/LoggingAspect.java
+++ src/main/java/com/learnings/log/LoggingAspect.java
@@ -6,31 +6,30 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 @Aspect
+@Slf4j
 @Component
 public class LoggingAspect {
 	
-	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
-
 	@Pointcut(value = "within(com.learnings..*)")
 	private void applicationPointCut() {}
 	
 	@Around(value = "applicationPointCut()")
 	public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-		if (LOGGER.isInfoEnabled()) {
-			LOGGER.info("Execution started :: {}.{}() with arguments = {}", 
+		if (log.isInfoEnabled()) {
+			log.info("Execution started :: {}.{}() with arguments = {}", 
 					joinPoint.getSignature().getDeclaringTypeName(),
 					joinPoint.getSignature().getName(),
 					Arrays.toString(joinPoint.getArgs()));
 		}
 		Object result = joinPoint.proceed();
-		if (LOGGER.isInfoEnabled()) {
-			LOGGER.info("Execution ended :: {}.{}() with result = {}",
+		if (log.isInfoEnabled()) {
+			log.info("Execution ended :: {}.{}() with result = {}",
 					joinPoint.getSignature().getDeclaringTypeName(),
 					joinPoint.getSignature().getName(), result);
 		}
